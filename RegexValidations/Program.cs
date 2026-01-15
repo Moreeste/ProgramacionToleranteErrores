0844374588b8838c9c150ec73b4b8406e0a3b14e
Esteban Rojas Huerta <esteban_moreeste@hotmail.com>
2026-01-14 23:59:09 -0600

    Regex validations

---
+++ RegexValidations/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+var patterName = @"^[a-zA-ZÀ-ÿ\s]{1,50}$";
+
+var regexName = new Regex(patterName);
+
+var name = "Héctor de León";
+
+if (regexName.IsMatch(name))
+{
+    Console.WriteLine($"{name} es un nombre válido");
+}
+else
+{
+    Console.WriteLine($"{name} no es un nombre válido");
+}
