ad19d020664cd2b7036b51422ebdd27b03948b95
Esteban Rojas Huerta <esteban_moreeste@hotmail.com>
2026-01-14 23:46:41 -0600

    Null reference

---
+++ NullReference/Program.cs
@@ -0,0 +1,15 @@
+﻿//Person person = null;
+Person person = new Person("Juan", new Address("CDMX"));
+
+//Console.WriteLine($"Hola {person.Name}");
+//Null conditional
+Console.WriteLine($"Hola {person?.Name}");
+
+//Null coalescing
+int length = person?.Name.Length ?? 0;
+Console.WriteLine($"El nombre tiene {length} letras");
+
+Console.WriteLine($"Hola {person?.Name} que es de la ciudad {person?.Address?.City}");
+
+record Address(string City);
+record Person(string Name, Address Address);
