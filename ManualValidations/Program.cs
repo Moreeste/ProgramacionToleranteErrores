1ef55ba4b04efd3908d5f45f2e290a94a59b8e21
Esteban Rojas Huerta <esteban_moreeste@hotmail.com>
2026-01-14 23:36:27 -0600

    Validaciones manuales

---
+++ ManualValidations/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Escribe tu edad: ");
+string sAge = Console.ReadLine();
+
+// Validación sintactica -> formatos
+if (!int.TryParse(sAge, out int age))
+{
+    Console.WriteLine($"[{sAge}] no tiene un formato de edad valido");
+    return;
+}
+
+// Validación semántica -> Coherencia
+if (age < 0 || age > 125)
+{
+    Console.WriteLine($"[{sAge}] la edad está fuera de un rango real");
+    return;
+}
+if (age < 18)
+{
+    Console.WriteLine("Debes ser mayor de edad");
+    return;
+}
+
+Console.WriteLine($"Tu edad {sAge} es correcta, continuar...");
