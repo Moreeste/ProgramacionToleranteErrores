2bf77210a0d089eab30845ec2d3504314a9606a5
Esteban Rojas Huerta <esteban_moreeste@hotmail.com>
2026-01-29 23:12:52 -0600

    Fallback

---
+++ IntroFallback/Program.cs
@@ -0,0 +1,25 @@
+﻿using Polly;
+
+var fallbackPolicy = Policy<decimal>.Handle<HttpRequestException>()
+                        .FallbackAsync(
+                            fallbackValue: 18.03m,
+                            onFallbackAsync: async (exception, context) =>
+                            {
+                                Console.WriteLine("Fallback: servicio no responde");
+                                Console.WriteLine($"Motivo {exception.Exception.Message}");
+                                await Task.CompletedTask;
+                            }
+                         );
+
+decimal dollarRate = await fallbackPolicy.ExecuteAsync(async () =>
+{
+    return await GetDollarRate();
+});
+
+Console.WriteLine($"La tasa del dólar es ${dollarRate}");
+
+async Task<decimal> GetDollarRate()
+{
+    Console.WriteLine("Solicitud a servicio");
+    throw new HttpRequestException();
+}
